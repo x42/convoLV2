16621ad0f55187fe424fa4cf89004813da0b4d7d
Robin Gareus <robin@gareus.org>
2012-08-26 18:17:17 +0200

    GPL-2 licensing

--- convolution.h
+++ convolution.h
@@ -1,4 +1,4 @@
-/* setBfree - DSP tonewheel organ
+/* convoLV2 -- LV2 convolution plugin
  *
  * Copyright (C) 2012 Robin Gareus <robin@gareus.org>
  *
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 #ifndef CONVOLUTION_H
